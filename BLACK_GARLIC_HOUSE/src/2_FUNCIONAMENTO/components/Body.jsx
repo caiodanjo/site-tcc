50328b051635db3a6458203166feb62cd0a079b0
caiodanjo <caio.danjo@gmail.com>
2023-10-13 17:24:39 -0300

    quase pronto

--- BLACK_GARLIC_HOUSE/src/2_FUNCIONAMENTO/components/Body.jsx
+++ BLACK_GARLIC_HOUSE/src/2_FUNCIONAMENTO/components/Body.jsx
@@ -8,7 +8,7 @@ function Body() {
   return (
     <main>
         <div className={styles.fundo}>
-            <h1>Vídeo do projeto:</h1>
+            <h1>Vídeo do Projeto:</h1>
 
             <figure className={styles.video}>
                 <img src={videoScreen} />
@@ -16,31 +16,37 @@ function Body() {
 
                 <h1>Informações Gerais</h1>
 
-                        <h2>Como preparar o alho?</h2>
                         <div className={styles.preparo}>
-                            <ul>
-                                <li>Tempo necessário para escurecer: 2 a 3 semanas;</li>
-                                <li>Temperatura: 60° a 77°C;</li>
-                                <li>Textura ideal: pegajosa;</li>
-                                <li>Gosto: adocicado.</li>
-                            </ul>
+                            <div>
+                                <h2>Como preparar o alho?</h2>
+
+                                <ul>
+                                    <li>Tempo necessário para escurecer: 2 a 3 semanas;</li>
+                                    <li>Temperatura: 60° a 77°C;</li>
+                                    <li>Textura ideal: pegajosa;</li>
+                                    <li>Gosto: adocicado.</li>
+                                </ul>
+                            </div>
 
                             <figure>
                                 <img src={estufa} />
                             </figure>
                         </div>
-
-                        <h2>Cuidados:</h2>
-                        <div className={styles.cuidados}>        
+                        
+                        <div className={styles.cuidados}> 
                             <figure>
                                 <img src={alhos} />
-                            </figure>
+                            </figure>   
 
-                            <ul>
-                                <li>Não há a adição de conservantes;</li>
-                                <li>Não lavar ou remover os bulbos;</li>
-                                <li>Umidade controlada.</li>
-                            </ul>
+                            <div>  
+                                <h2>Cuidados:</h2>
+
+                                <ul>
+                                    <li>Não há a adição de conservantes;</li>
+                                    <li>Não lavar ou remover os bulbos;</li>
+                                    <li>Umidade controlada.</li>
+                                </ul>
+                            </div>
                         </div>
 
             </div>
